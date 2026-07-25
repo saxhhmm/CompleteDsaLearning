40c32df223b510a6c42d193945a58c86d9270ed8
saxhhmm <saksham2413049@akgec.ac.in>
2026-07-25 22:42:08 +0530

    Time: 5 ms (41.44%), Space: 46.9 MB (28.88%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,30 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
+        
+        for (ListNode node : lists) {
+            if (node != null) {
+                minHeap.add(node);
+            }
+        }
+        
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        
+        while (!minHeap.isEmpty()) {
+            ListNode smallestNode = minHeap.poll();
+            current.next = smallestNode;
+            current = current.next;
+            
+            if (smallestNode.next != null) {
+                minHeap.add(smallestNode.next);
+            }
+        }
+        
+        return dummy.next;
+    }
+}
