c091e6e0035aae83c5678111e4769e8064d68b59
saxhhmm <saksham2413049@akgec.ac.in>
2026-09-02 23:06:25 +0530

    Time: 0 ms (100%), Space: 44.8 MB (94.09%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // Since we can always force the array to be uniformly odd (Even - Odd = Odd) 
+        // or keep it uniform if it already is, it is always possible.
+        return true;
+    }
+}
